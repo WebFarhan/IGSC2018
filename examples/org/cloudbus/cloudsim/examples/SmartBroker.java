804f6a61896427c6bc5decfaa173e1780c5a74c7
Hussain Razin Farhan <c00303945@moshenlab1.win.louisiana.edu>
2018-05-10 23:55:32 -0500

    IGSC project uploaded

---
+++ examples/org/cloudbus/cloudsim/examples/SmartBroker.java
@@ -0,0 +1,14 @@
+package org.cloudbus.cloudsim.examples;
+
+import org.cloudbus.cloudsim.DatacenterBroker;
+
+public class SmartBroker extends DatacenterBroker{
+
+	public SmartBroker(String name) throws Exception {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
